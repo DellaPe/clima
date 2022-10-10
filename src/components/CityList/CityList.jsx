66ae00afa61b5afb6ca676adb91db8e6e6193c6d
Pedro Dellatorre <111153020+DellaPe@users.noreply.github.com>
2022-10-10 15:23:15 -0300

    9no Commit Tapal

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from 'axios'
 import convertUnit from 'convert-units'
-import Typography from "@mui/material/Typography";
 
 import Grid from "@mui/material/grid";
 import CityInfo from "./../CityInfo";
 import Weather from "./../Weather";
 import {Alert, List, ListItem} from "@mui/material"
 
+const getCityCode = (city, countryCode) => `${city}-${countryCode}`
+
 const renderCityAndCountry = (eventOnClickCity) => (cityAndCountry, weather) => {
-  const { city, country } = cityAndCountry;
+  const { city, countryCode,country } = cityAndCountry;
   
   //sm={8} para tamaños sm para arriba 8 + 4, para el resto 12
   return (
-    <ListItem  button key={city} onClick={eventOnClickCity}>
+    <ListItem  button key={getCityCode(city, countryCode)} onClick={eventOnClickCity}>
       <Grid container justifyContent="center" alignItems="center">
         <Grid item md={8} sm={12} xs={12}>
           <CityInfo city={city} country={country} />
@@ -39,16 +40,38 @@ const CityList = ({ cities, onClickCity }) => {
   //Aca invicamos anuestra api
   useEffect(() => {
     
-    const setWeather = (city, country) => {
+    const setWeather = async (city, countryCode) => {
       const apiKey = "b1907a1349b8dc3a835d896d914dc02c";
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-      axios
-      .get(url) //Ingresa a cada ciudad y me trae los datos
-      .then(response =>{//Lo que venga en then es porque la peticion fue exitosa
+      
+      try {
+        const response = await axios.get(url) //Ingresa a cada ciudad y me trae los datos
         const { data } = response
         const temperature = Number(convertUnit(data.main.temp).from("K").to("C").toFixed(0))
         const state = "sun"
-        setAllWeather(allWeather =>({ ...allWeather, [`${city}-${country}`]: { temperature, state } })) //[Buenas Aires-Argentina]: { datos }
+        console.log(state)
+
+        const cityProps = getCityCode(city, countryCode)
+        const valueProps = { temperature, state }
+
+        setAllWeather(allWeather =>({ ...allWeather, [cityProps]: valueProps })) //[Buenas Aires-Argentina]: { datos }
+      } catch (error) {
+        if(error.response) {
+          setError("Error de servidor")
+        } else if (error.request){ //Error 2: El server no responde, no llegamos al server
+          setError(" Error de coneccion")
+        } else{ //Error 3: Que no conocemos. 
+          setError("Error imprevisto")
+        }
+      }
+      
+      
+      
+      
+      /*.then(response =>{//Lo que venga en then es porque la peticion fue exitosa
+        const { data } = response
+        const temperature = Number(convertUnit(data.main.temp).from("K").to("C").toFixed(0))
+        const state = "sun"
       })
       .catch( error => {
         //Error 1: El servidor responde algo que no queremos. 
@@ -59,11 +82,11 @@ const CityList = ({ cities, onClickCity }) => {
         } else{ //Error 3: Que no conocemos. 
           setError("Error imprevisto")
         }
-      })
+      })*/
     }
     //Recorremos el arreglo de ciuddades que carguemos
-    cities.forEach(({ city, country}) => {
-      setWeather(city, country)
+    cities.forEach(({ city, countryCode }) => {
+      setWeather(city, countryCode)
     });
 
 
@@ -78,14 +101,13 @@ const CityList = ({ cities, onClickCity }) => {
         <Alert onClose={()=>setError(null)} variant="outlined" severity="error">
           {error}
         </Alert>
-      
       }
       <List>
         {
           //renderCityAndCountry se transforma en una func que tetorna otra fucn
           cities.map((cityAndCountry) =>
             renderCityAndCountry(onClickCity)(cityAndCountry, 
-              allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`])
+              allWeather[getCityCode(cityAndCountry.city, cityAndCountry.countryCode)])
           )
         }
       </List>
---
+++ src/generic/ErrorBoundary/ErrorBoundary.strories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import ErrorBoundary from "./ErrorBoundary"
+
+export default {
+    component: ErrorBoundary,
+    title: "Error Boundary"
+}
+
+const ErrorBoundary = () => <ErrorBoundary /> 
