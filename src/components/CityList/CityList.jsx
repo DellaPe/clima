9a10573d4a363e8430bf3c99a28ec898fd2007a7
DellaPe <dellatorre.pe@gmail.com>
2022-12-25 21:34:54 -0300

    Inicio de mejora de renderizado

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,82 +1,63 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Grid from "@mui/material/grid";
-import CityInfo from "./../CityInfo";
-import Weather from "./../Weather";
-import { Alert, List, ListItem } from "@mui/material";
+import { Alert, List } from "@mui/material";
 //utils
 import getCityCode from "./../../utils/getCityCode"
 //hooks
 import useCityList from "./../../hooks/useCityList";
+// Nuevo componente 
+import CityListItem from "../CityListItem/CityListItem";
 
 const renderCityAndCountry =
-    (eventOnClickCity) => (cityAndCountry, weather) => {
-        const { city, country } = cityAndCountry;
-
-        //sm={8} para tamaños sm para arriba 8 + 4, para el resto 12
-        return (
-            <ListItem
-                button
-                key={getCityCode(city, country)}
-                onClick={() => eventOnClickCity(city, country, weather.temperature, weather.state, weather.humidity, weather.speed)}
-            >
-                <Grid container justifyContent="center" alignItems="center">
-                    <Grid item md={8} sm={12} xs={12}>
-                        <CityInfo city={city} country={country} />
-                    </Grid>
-                    <Grid item md={4} sm={12} xs={12}>
-                        <Weather
-                            temperature={weather && weather.temperature}
-                            state={weather && weather.state}
-                        />
-                    </Grid>
-                </Grid>
-            </ListItem>
-        );
-    };
+  (eventOnClickCity) => (cityAndCountry, weather) => {
+    const { city, country } = cityAndCountry
+    return (
+      <CityListItem key={getCityCode(city, country)} eventOnClickCity={eventOnClickCity} weather={weather} {...cityAndCountry} />
+    );
+  };
 
 const CityList = ({ cities, onClickCity, actions, dataAll }) => {
-    const {allWeather} = dataAll
-    const { error, setError } = useCityList(cities, allWeather, actions) //Elimino el allWeather que habia aca porque me viene como dato
-    return (
-        <div>
-            {
-                //onClose para que salte la x
-                error && (
-                    <Alert
-                        onClose={() => setError(null)}
-                        variant="outlined"
-                        severity="error"
-                    >
-                        {error}
-                    </Alert>
-                )
-            }
-            <List>
-                {
-                    //renderCityAndCountry se transforma en una func que tetorna otra fucn
-                    cities.map((cityAndCountry) =>
-                        renderCityAndCountry(onClickCity)(
-                            cityAndCountry,
-                            allWeather[
-                            getCityCode(cityAndCountry.city, cityAndCountry.country)
-                            ]
-                        )
-                    )
-                }
-            </List>
-        </div>
-    );
+  const { allWeather } = dataAll
+  const { error, setError } = useCityList(cities, allWeather, actions) //Elimino el allWeather que habia aca porque me viene como dato
+  return (
+    <div>
+      {
+        //onClose para que salte la x
+        error && (
+          <Alert
+            onClose={() => setError(null)}
+            variant="outlined"
+            severity="error"
+          >
+            {error}
+          </Alert>
+        )
+      }
+      <List>
+        {
+          //renderCityAndCountry se transforma en una func que tetorna otra fucn
+          cities.map((cityAndCountry) =>
+            renderCityAndCountry(onClickCity)(
+              cityAndCountry,
+              allWeather[
+              getCityCode(cityAndCountry.city, cityAndCountry.country)
+              ]
+            )
+          )
+        }
+      </List>
+    </div>
+  );
 };
 
 CityList.propTypes = {
-    cities: PropTypes.arrayOf(
-        PropTypes.shape({
-            city: PropTypes.string.isRequired,
-            coutry: PropTypes.string.isRequired,
-        })
-    ).isRequired,
-    onClickCity: PropTypes.func.isRequired,
+  cities: PropTypes.arrayOf(
+    PropTypes.shape({
+      city: PropTypes.string.isRequired,
+      coutry: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onClickCity: PropTypes.func.isRequired,
 };
 
-export default CityList;
+export default React.memo(CityList); // Se puede hacer aca el memo 
---
+++ src/components/CityListItem/CityListItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Grid from "@mui/material/grid";
+import CityInfo from "./../CityInfo";
+import Weather from "./../Weather";
+import { ListItem } from "@mui/material";
+const areEqual = (prev, next) = {
+    debugger
+    console.log({})
+}
+const CityListItem = ({ city, country, weather, eventOnClickCity }) => {
+    return (
+        //sm={8} para tamaños sm para arriba 8 + 4, para el resto 12
+        <ListItem
+            button
+            onClick={() => eventOnClickCity(city, country, weather.temperature, weather.state, weather.humidity, weather.speed)}
+        >
+            <Grid container justifyContent="center" alignItems="center">
+                <Grid item md={8} sm={12} xs={12}>
+                    <CityInfo city={city} country={country} />
+                </Grid>
+                <Grid item md={4} sm={12} xs={12}>
+                    <Weather
+                        temperature={weather && weather.temperature}
+                        state={weather && weather.state}
+                    />
+                </Grid>
+            </Grid>
+        </ListItem>
+    );
+}
+
+export default React.memo(CityListItem);
